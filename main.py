9055b48317f307bd337181e0b8cdeffc0c34d622
RuixiangLi <U1388321@umail.utah.edu>
2023-10-03 15:09:50 -0600

    my solution for step 1

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ def read_synonym():
     synonym_dic = {}
     with open("test-synonyms.txt") as file:
         for things in file:
-            key = things.split(':')[0]
-            value = things.split(':')[1]
-            synonym_dic[key] = value
+            things_without_space = things.strip()
+            key = things_without_space.split(':')[0]
+            value = things_without_space.split(':')[1]
+            value_list = value.split(",")
+            synonym_dic[key] = value_list
     return synonym_dic
+
+
 test_myfunction = read_synonym()
 print(test_myfunction)
 
-#class Spineer:
-    #def __init__(self):
+# class Spinner:
+    # def __init__(self):
