e8e25b7b9b488f714786c6cc5e7ad58873429627
RuixiangLi <U1388321@umail.utah.edu>
2023-10-03 18:30:19 -0600

    this is my attempt for trying to replace words

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-# class Spinner:
-#     def __init__(self):
-def read_synonym():
-    synonym_dic = {}
-    with open("test-synonyms.txt") as file:
-        for things in file:
-            things_without_space = things.strip()
-            key = things_without_space.split(':')[0]
-            value = things_without_space.split(':')[1]
-            value_list = value.split(",")
-            synonym_dic[key] = value_list
-    return synonym_dic
+
+import string
+class Spinner:
+    def __init__(self):
+        self.replace_dic = self.read_synonym()
+
+    def read_synonym(self):
+        synonym_dic = {}
+        with open("test-synonyms.txt") as file:
+            for things in file:
+                things_withouspace = things.strip()
+                key = things_withouspace.split(':')[0]
+                value = things_withouspace.split(':')[1]
+                value_list = value.split(",")
+                synonym_dic[key] = value_list
+        return synonym_dic
+    def look_for_keyword(self):
+        replaced_string = []
+        for word in self.replace_dic:
+            if word in self.replace_dic:
+                synonym_list =self.replace_dic[word]
+                replacement = synonym_list
+                replaced_string.append(replacement)
+            else:
+                replaced_string.append(word)
+        return replaced_string
